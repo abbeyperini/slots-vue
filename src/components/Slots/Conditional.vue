5fcc6355f49e6cc2e0a6bb13ca30ba7f22f27db7
abbeyperini <abbeyperini@gmail.com>
2024-09-24 15:52:26 -0400

    Don't need fallback content

--- src/components/Slots/Conditional.vue
+++ src/components/Slots/Conditional.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="$slots.club" class="club-container">
     <img src="../../assets/the-window.jpg" alt="the window" />
-    <slot name="club"><img src="../../assets/confused-lil-jon.jpg" alt="lil jon saying 'what!?'" /></slot>
+    <slot name="club"></slot>
     <img src="../../assets/the-wall.png" alt="the wall" />
   </div>
 </template>
